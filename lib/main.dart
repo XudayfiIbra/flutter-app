9ed26532e02e9eea4eec54649cb0a0d73b825164
xudayfi <xudayfiibraahim8@gmail.com>
2023-12-29 19:13:53 +0300

    practiced the most basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,106 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
         appBar: AppBar(
-          elevation: 0,
-          title: const Text('WACIYE STORE'),
-          backgroundColor: Colors.deepPurple,
-          leading: const Icon(Icons.menu),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.person_2)),
-          ],
-          toolbarHeight: 50,
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20)),
-            padding: const EdgeInsets.all(25),
-            child: const Icon(
-              Icons.check_circle,
-              color: Colors.red,
-              size: 40,
+          title: const Text(
+            'Home',
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
             ),
           ),
+          centerTitle: true,
+          leading: const Icon(
+            Icons.drag_handle,
+            color: Colors.deepPurple,
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(
+                Icons.notifications_active_sharp,
+                color: Colors.deepPurple,
+              ),
+            )
+          ],
+          elevation: 0,
+          backgroundColor: Colors.white,
+          toolbarHeight: 70,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Text(
+                'Hi Jenifer!',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Color.fromARGB(255, 38, 1, 102),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Text(
+                'Good Morning',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromRGBO(55, 10, 134, 1),
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              width: 350,
+              height: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 38, 1, 102),
+                    width: 2,
+                  )),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: const Text(
+                      'Ongoing Projects',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color.fromRGBO(55, 10, 134, 1),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 130),
+                    child: const Text(
+                      'view all',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromRGBO(55, 10, 134, 1),
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
